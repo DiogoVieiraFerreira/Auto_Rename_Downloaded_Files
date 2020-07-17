c481ff398fca773b946456b287e5a0af51019323
Diogo <diogof-6@hotmail.com>
2020-07-17 18:18:33 +0200

    create project

---
+++ Auto_Rename_Downloaded_Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Auto_Rename_Downloaded_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
